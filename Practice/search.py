bf12788c3050e0fa6062061152df759bbc060881
Niivas <nivas6900@gmail.com>
2023-09-17 22:26:52 +0530

    Did some feature work

--- Practice/search.py
+++ Practice/search.py
@@ -11,9 +11,49 @@ BestContinuingMangas = [ 'One Piece', 'Jujutsu Kaisen', 'My Hero Academia', 'Cha
                          'Bungo Stray Dogs', 'Kingdom', 'Berserk', 'Hunter x Hunter', 'A Certain Scientific Railgun',
                          'Vagabond' ]
 
-searchPage = requests.get(MangaSearchUrl + " ")
-SearchSoup = BeautifulSoup(searchPage.content, "lxml")
-print(SearchSoup)
+siteAcceptedNames = { 'One Piece': 'one-piece', 'Jujutsu Kaisen': 'jujutsu-kaisen',
+                      'My Hero Academia': 'my-hero-academia', 'Chainsaw Man': 'chainsaw-man',
+                      'Dragon Ball Super': 'dragon-ball-super', 'Kaiju No. 8': 'kaiju-no',
+                      'Boruto: Naruto Next Generations': 'boruto-naruto-next-generations',
+                      'Spy × Family': 'spy-x-family', 'Kingdom': 'kingdom', 'Berserk': 'berserk',
+                      'Hunter x Hunter': 'hunter-x-hunter', 'Vagabond': 'vagabond' }
+
+"""
+def parseName(name):
+    name = name.lower()
+    res = []
+    for char in name:
+        if char.isalnum():
+            res.append(char)
+    return "".join(res)
+
+
+def parseUrl(url):
+    n = len(url)
+    for i in range(n - 2, -1, -1):
+        if url[ i ] == '/':
+            return url[ i + 1:-1 ]
+
+
+for manga in BestContinuingMangas:
+    searchPage = requests.get(MangaSearchUrl + manga)
+    searchSoup = BeautifulSoup(searchPage.content, "lxml")
+    if searchSoup.find('h1', class_="page-title"):
+        continue
+    searchResults = searchSoup.find_all('h4')
+
+    for result in searchResults:
+        elementA = result.find('a')
+        title = elementA.text
+        mangaUrl = elementA.get('href', None)
+        if not title or not mangaUrl:
+            continue
+        if parseName(title) == parseName(manga):
+            siteAcceptedNames[ manga ] = parseUrl(mangaUrl)
+            break
+
+print(siteAcceptedNames)
+"""
 """
 fetchPage = requests.get(MangaFetchUrl)
 fetchSoup = BeautifulSoup(fetchPage.content, 'lxml')
