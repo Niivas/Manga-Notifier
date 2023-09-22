2d0cdf15f90f30f1ffb85cbb89e57ddf4cd463bc
Niivas <nivas6900@gmail.com>
2023-09-22 16:23:30 +0530

    Refactored code

--- Practice/main.py
+++ Practice/main.py
@@ -7,67 +7,6 @@ import os
 baseUrl = "https://mangajuice.com/manga/"
 MangaUpdatesUrl = 'https://mangajuice.com/updates/'
 
-"""
-mangas = {
-    'One Piece': {
-        'siteAcceptedName': 'one-piece', 'latestChapter': 1092.0,
-        'latestChapterLink': 'https://mangajuice.com/chapter/one-piece-chapter-1092/',
-        'chaptersAddedSinceYouLastRead': 0.0
-        }, 'Jujutsu Kaisen': {
-        'siteAcceptedName': 'jujutsu-kaisen', 'latestChapter': 235.0,
-        'latestChapterLink': 'https://mangajuice.com/chapter/jujutsu-kaisen-chapter-235/',
-        'chaptersAddedSinceYouLastRead': 0.0
-        }, 'My Hero Academia': {
-        'siteAcceptedName': 'my-hero-academia', 'latestChapter': 400.0,
-        'latestChapterLink': 'https://mangajuice.com/chapter/my-hero-academia-chapter-400/',
-        'chaptersAddedSinceYouLastRead': 0.0
-        }, 'Chainsaw Man': {
-        'siteAcceptedName': 'chainsaw-man', 'latestChapter': 142.0,
-        'latestChapterLink': 'https://mangajuice.com/chapter/chainsaw-man-chapter-142/',
-        'chaptersAddedSinceYouLastRead': 0.0
-        }, 'Dragon Ball Super': {
-        'siteAcceptedName': 'dragon-ball-super', 'latestChapter': 96.0,
-        'latestChapterLink': 'https://mangajuice.com/chapter/dragon-ball-super-chapter-96/',
-        'chaptersAddedSinceYouLastRead': 0.0
-        }, 'Kaiju No. 8': {
-        'siteAcceptedName': 'kaiju-no', 'latestChapter': 93.0,
-        'latestChapterLink': 'https://mangajuice.com/chapter/kaiju-no-chapter-93/', 'chaptersAddedSinceYouLastRead': 0.0
-        }, 'Boruto: Naruto Next Generations': {
-        'siteAcceptedName': 'boruto-naruto-next-generations', 'latestChapter': 80.0,
-        'latestChapterLink': 'https://mangajuice.com/chapter/boruto-chapter-80/', 'chaptersAddedSinceYouLastRead': 0.0
-        }, 'Spy X Family': {
-        'siteAcceptedName': 'spy-x-family', 'latestChapter': 87.0,
-        'latestChapterLink': 'https://mangajuice.com/chapter/spy-x-family-chapter-87/',
-        'chaptersAddedSinceYouLastRead': 0.0
-        }, 'Kingdom': {
-        'siteAcceptedName': 'kingdom', 'latestChapter': 743.0,
-        'latestChapterLink': 'https://mangajuice.com/chapter/kingdom-chapter-743/', 'chaptersAddedSinceYouLastRead': 0.0
-        }, 'Berserk': {
-        'siteAcceptedName': 'berserk', 'latestChapter': 373.0,
-        'latestChapterLink': 'https://mangajuice.com/chapter/berserk-chapter-373/', 'chaptersAddedSinceYouLastRead': 0.0
-        }, 'Hunter x Hunter': {
-        'siteAcceptedName': 'hunter-x-hunter', 'latestChapter': 400.0,
-        'latestChapterLink': 'https://mangajuice.com/chapter/hunter-x-hunter-chapter-400/',
-        'chaptersAddedSinceYouLastRead': 0.0
-        }, 'Vagabond': {
-        'siteAcceptedName': 'vagabond', 'latestChapter': 327.0,
-        'latestChapterLink': 'https://mangajuice.com/chapter/vagabon-chapter-327/', 'chaptersAddedSinceYouLastRead': 0.0
-        }, 'Komi Cant Communicate': {
-        'siteAcceptedName': 'komi-san-wa-komyushou-desu', 'latestChapter': 419.0,
-        'latestChapterLink': 'https://mangajuice.com/chapter/komi-san-chapter-419/',
-        'chaptersAddedSinceYouLastRead': 0.0
-        }, 'Rent a Girlfriend': {
-        'siteAcceptedName': 'rent-a-girlfriend', 'latestChapter': 298.0,
-        'latestChapterLink': 'https://mangajuice.com/chapter/rent-a-girlfriend-chapter-298/',
-        'chaptersAddedSinceYouLastRead': 0.0
-        }, 'More Than a Married Couple But Not Lovers': {
-        'siteAcceptedName': 'fuufu-ijou-koibito-miman', 'latestChapter': 64.0,
-        'latestChapterLink': 'https://mangajuice.com/chapter/fuufu-ijou-koibito-miman-chapter-64/',
-        'chaptersAddedSinceYouLastRead': 0.0
-        }
-    }
-"""
-
 
 def fetchLatestChapter(latestChapterLink):
     n = len(latestChapterLink)
@@ -149,6 +88,7 @@ ti = time.time()
 fetchMangasInfo(mangas)
 tf = time.time()
 print(f"Time took for fetching updates of {len(mangas)} mangas: {tf - ti} seconds")
+previousMangaUpdatesFile.close()
 
 os.chdir(r'C:\Users\Nivas Reddy\Desktop\Manga-Notifier\results')
 ti = time.time()
--- Practice/scratch.py
+++
@@ -1,7 +0,0 @@
-import os
-import json
-
-file = open(r'C:\Users\Nivas Reddy\Desktop\Manga-Notifier\results\Latest Manga Updates.txt', 'r')
-
-d = json.loads(file.read())
-print(type(d))
