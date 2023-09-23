695e31c445e7cdc593d9d463fd110831763db77a
Niivas <nivas6900@gmail.com>
2023-09-23 12:21:49 +0530

    Testing Git Automation

---
+++ Practice/gitAutomated.py
@@ -0,0 +1,10 @@
+import subprocess
+
+msg = input('Type the commit message (+ ENTER):')
+repo_directory = r'C:\Users\Nivas Reddy\Desktop\Manga-Notifier'
+
+subprocess.run(["git", "add", "."], cwd=repo_directory)
+# commit file
+subprocess.run(["git", "commit", "-m", msg], cwd=repo_directory)
+# push
+subprocess.run(["git", "push"], cwd=repo_directory)
