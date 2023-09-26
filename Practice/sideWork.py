bb1127b60591e504bfebe949d3be861d289057a0
Niivas <nivas6900@gmail.com>
2023-09-26 20:39:06 +0530

    Last Fetched: 20:39 2023-09-26

---
+++ Practice/sideWork.py
@@ -0,0 +1,12 @@
+import json
+
+with open(r'C:\Users\Nivas Reddy\Desktop\Manga-Notifier\results\Latest Manga Updates.txt','r') as file:
+    mangas = json.loads(file.read())
+
+for manga in mangas:
+    isFavorite = input(f"is {manga} your favorite (yes/no): ")
+    mangas[manga]["isFavorite"] = isFavorite
+
+with open(r'C:\Users\Nivas Reddy\Desktop\Manga-Notifier\results\Latest Manga Updates.txt', 'w') as file:
+    file.write(json.dumps(mangas, indent=4))
+
