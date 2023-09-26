d984acd94fb3118bf0551f2ef056229ca966d9ba
Niivas <nivas6900@gmail.com>
2023-09-26 08:56:12 +0530

    Last Fetched: 08:56 2023-09-26

--- Practice/gitAutomated.py
+++ Practice/gitAutomated.py
@@ -5,6 +5,7 @@ commit_message = f'Last Fetched: {datetime.now().strftime("%H:%M %Y-%m-%d")}'
 
 repo_directory = r'C:\Users\Nivas Reddy\Desktop\Manga-Notifier'
 
+
 subprocess.run(["git", "add", "*"], cwd=repo_directory)
 # commit file
 subprocess.run(["git", "commit", "-m", commit_message], cwd=repo_directory)
