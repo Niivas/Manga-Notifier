83eb93cc895abc5202c6282c91020261d86881b8
Niivas <nivas6900@gmail.com>
2023-10-15 18:39:38 +0530

    Last Fetched: 18:39 2023-10-15

--- Practice/sideWork.py
+++ Practice/sideWork.py
@@ -1,6 +1,6 @@
 import json
 
-with open(r'C:\Users\Nivas Reddy\Desktop\Manga-Notifier\results\Latest Manga Updates.txt','r') as file:
+with open(r'C:\Users\Nivas Reddy\Desktop\Manga-Notifier\results\Latest Manga Updates.txt', 'r') as file:
     mangas = json.loads(file.read())
 
 for manga in mangas:
@@ -9,4 +9,3 @@ for manga in mangas:
 
 with open(r'C:\Users\Nivas Reddy\Desktop\Manga-Notifier\results\Latest Manga Updates.txt', 'w') as file:
     file.write(json.dumps(mangas, indent=4))
-
