f9e51a9840b369c9ef3743e4626048ebb5a286bd
Niivas <nivas6900@gmail.com>
2023-10-23 18:28:24 +0530

    Added images for all mangas-2

--- main/sideWork.py
+++ main/sideWork.py
@@ -11,13 +11,25 @@ for manga in mangas:
     isFavorite = input(f"is {manga} your favorite (yes/no): ")
     mangas[manga]["isFavorite"] = isFavorite
 """
+
+
+def parseName(name):
+    if "\u2019" in name:
+        return name.split("\u2019")[0]
+    if "'" in name:
+        return name.split("'")[0]
+    return name
+
+
 for manga in mangas:
-    searchPage = requests.get(searchUrl(manga))
+    searchPage = requests.get(searchUrl(parseName(manga)))
     soup = LexborHTMLParser(searchPage.text)
     if soup.css_first('div.not-found-content'):
         mangas[manga]["image"] = ""
+        print(f"Could not find {manga}")
         continue
     mangas[manga]['image'] = soup.css('div.search-wrap')[0].css_first('img').attrs.get('data-src')
+    print(f"Found {manga}")
 
 with open(r'C:\Users\Nivas Reddy\Desktop\Github files\Manga-Notifier\results\Latest Manga Updates.txt', 'w') as file:
     file.write(json.dumps(mangas, indent=4))
