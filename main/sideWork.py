3f46442416f6e4a7e4bcbe41cd9f248738d33f44
Niivas <nivas6900@gmail.com>
2023-10-23 17:49:32 +0530

    Added images for all manga

--- main/sideWork.py
+++ main/sideWork.py
@@ -1,11 +1,23 @@
 import json
+import requests
+from selectolax.lexbor import LexborHTMLParser
 
-with open(r'C:\Users\Nivas Reddy\Desktop\Manga-Notifier\results\Latest Manga Updates.txt', 'r') as file:
+searchUrl = lambda x: f"https://www.mangaread.org/?s={x}&post_type=wp-manga"
+
+with open(r'C:\Users\Nivas Reddy\Desktop\Github files\Manga-Notifier\results\Latest Manga Updates.txt', 'r') as file:
     mangas = json.loads(file.read())
-
+"""
 for manga in mangas:
     isFavorite = input(f"is {manga} your favorite (yes/no): ")
     mangas[manga]["isFavorite"] = isFavorite
+"""
+for manga in mangas:
+    searchPage = requests.get(searchUrl(manga))
+    soup = LexborHTMLParser(searchPage.text)
+    if soup.css_first('div.not-found-content'):
+        mangas[manga]["image"] = ""
+        continue
+    mangas[manga]['image'] = soup.css('div.search-wrap')[0].css_first('img').attrs.get('data-src')
 
-with open(r'C:\Users\Nivas Reddy\Desktop\Manga-Notifier\results\Latest Manga Updates.txt', 'w') as file:
+with open(r'C:\Users\Nivas Reddy\Desktop\Github files\Manga-Notifier\results\Latest Manga Updates.txt', 'w') as file:
     file.write(json.dumps(mangas, indent=4))
